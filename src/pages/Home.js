8d86db23882c5ab3d232f40a033384c38f5cd90b
ggombee <minniemice98@gmail.com>
2022-03-26 07:12:57 +0900

    add: og options

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -15,7 +15,7 @@ const Home = () => {
   return (
     <>
       <Container>
-        <Header>😼 집사 전용 판별기 😼</Header>
+        <Header>😼 예비집사 판별기 😼</Header>
         <Wrapper>
           <Title>나에게 맞는 주인님은?!</Title>
           <LogoImage>
